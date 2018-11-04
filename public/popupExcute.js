86bb84ca94f0a25fd1f5ace9a9a2ef38dff8b313
ManhNguyen98 <nguyenthietmanh98@gmail.com>
2018-11-04 10:05:31 +0700

    chỉnh sửa pop up

--- public/popupExcute.js
+++ public/popupExcute.js
@@ -1,12 +1,26 @@
 var socket = io("http://localhost:3000");
 
 socket.on("tuvantinhcam-chat",function(data){
-    var message = "<div class='messages'>" + data +"<div>";
+    var time = getCurrentTime();
+    var message = "<li class='messages'><div class='messages-container-o'><div class='other-avt'><img src = '/images/avt1.jpg'></div><div class = 'o-message-text'>" + data +"</div></div><div class = 'message-footer o-footer'>"+time+"</div></li>"
     $(".pop-up-mess").append(message);
 });
-
+function getCurrentTime(){
+    var d = new Date();
+    var day = d.getDate();
+    if (day < 10 ) day = "0" + day;
+    var month = d.getMonth() + 1;
+    if (month<10) month = "0" + month;
+    var hours = d.getHours();
+    if (hours<10) hour = "0"+hours;
+    var minute = d.getMinutes();
+    if (minute<10) minute = "0" + minute;
+    var time = day + "/"+month+"/" + d.getFullYear() + " " + hours + ":" + minute;
+    return time;
+}
 socket.on("your-mess",function(data){
-    var message = "<div class='messages u-messages'>" + data +"<div>";
+    var time = getCurrentTime();
+    var message = "<li class='messages'><div class='messages-container-u'><div class='user-avt'><img src = '/images/avt1.jpg'></div><div class = 'u-message-text'>" + data +"</div></div><div class = 'message-footer u-footer'>"+time+"</div></li>"
     $(".pop-up-mess").append(message);
 });
 
@@ -22,7 +36,7 @@ $(document).ready(function(){
         $("#close").click(function(){
             $(".pop-up-box").hide();
         })
-    
+        $("#mess").focus();
         $(".pop-up-head").click(function(){
             $(".pop-up-box").hide();
         })
@@ -33,6 +47,13 @@ $(document).ready(function(){
             socket.emit('tuvantinhcam-chatting',$("#mess").val());
             $("#mess").val("");
         })
+
+        $("#mess").keyup(function(e){
+            if((event.keyCode === 13 ) && ($("#mess").val()!=null) && ($("#mess").val()!="")){
+                $(".btnSend-tuvantinhcam").click();
+            }
+        });
+
     });
 
 
