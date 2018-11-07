8d0e4d333ea1e3dce67e15bcb359d44feb8d6dc0
ManhNguyen98 <nguyenthietmanh98@gmail.com>
2018-11-07 12:51:28 +0700

    Ẩn hiện status và reload khi post

--- public/homeControl.js
+++ public/homeControl.js
@@ -7,6 +7,7 @@ socket.on('your-name',function(data){
     $(".fullname").html("<span>"+name+"</span>");
     $(".userName").html("@"+username);
 });
+function load_status(){
 socket.on('your-status',function(status){
     var name = user.firstName + " " + user.lastName;
     var userstatus = status.split("```");
@@ -15,11 +16,24 @@ socket.on('your-status',function(status){
         var time = info[0];
         var text = info[1];
         var element = "<div class='newPost'><div class='userCard'><img src='/images/avt1.jpg'><div class='newFeedFullName'><span>"+name+"</span>";
-        var element = element + "<div class='dropdown'><button data-toggle='dropdown'><i class='glyphicon glyphicon-option-vertical'></i></button><ul class='dropdown-menu'><li id='statusHide'>Ẩn <i class='glyphicon glyphicon-eye-close'></i></li><li id='statusDel'>Xóa <i class='glyphicon glyphicon-trash'></i></li></ul></div></div>";
-        var element = element + "<span class='timer'>"+time+"</span></div><div class='userText'><br><p>"+text+"</p></div></div>";
+        var element = element + "<div class='dropdown'><button data-toggle='dropdown'><i class='glyphicon glyphicon-option-vertical'></i></button><ul class='dropdown-menu'><li id ='"+i+"' onclick='statusHide("+user.userName+i+","+i+")'>Ẩn <i class='glyphicon glyphicon-eye-close'></i></li><li>Xóa <i class='glyphicon glyphicon-trash'></i></li></ul></div></div>";
+        var element = element + "<span class='timer'>"+time+"</span></div><div class='userText'><br><p id ='"+user.userName+i+"'>"+text+"</p></div></div>";
         $(".newFeed").prepend(element);
     }
 })
+}
+function statusHide(id,i){
+    var x = document.getElementById(id);
+    var btn = document.getElementById(i);
+    if (x.style.display != "none"){
+        x.style.display = "none";
+        btn.innerHTML = "Hiện <i class='glyphicon glyphicon-eye-open'></i>";
+    }
+    else{
+        x.style.display = "block";
+        btn.innerHTML = "Ẩn <i class='glyphicon glyphicon-eye-close'></i>";
+    }
+}
 function getCurrentTime(){
     var d = new Date();
     var day = d.getDate();
@@ -33,21 +47,21 @@ function getCurrentTime(){
     var time = day + "/"+month+"/" + d.getFullYear() + " " + hours + ":" + minute;
     return time;
 }
+function createId(){
+    var d = new Date();
+    return "" + d.getDate() + d.getMonth() + d.getFullYear() + d.getHours() + d.getMinutes() + d.getMilliseconds();
+}
 function postStatus(){
-    var name = user.firstName + " " + user.lastName;
     var timer = getCurrentTime();
     if ($(".statusText").val()!= null && $(".statusText").val()!=""){
-    var element = "<div class='newPost'><div class='userCard'><img src='/images/avt1.jpg'><div class='newFeedFullName'><span>"+name+"</span>";
-    var element = element + "<div class='dropdown'><button data-toggle='dropdown'><i class='glyphicon glyphicon-option-vertical'></i></button><ul class='dropdown-menu'><li id='statusHide'>Ẩn <i class='glyphicon glyphicon-eye-close'></i></li><li id='statusDel'>Xóa <i class='glyphicon glyphicon-trash'></i></li></ul></div></div>";
-    var element = element + "<span class='timer'>"+timer+"</span></div><div class='userText'><br><p>"+$(".statusText").val()+"</p></div></div>";
-    $(".newFeed").prepend(element);
     var status = timer + "``" + $(".statusText").val();
     //luu status vao csdl
     socket.emit("save-status",user,status);
-    $(".statusText").val("");
+    location.reload();
 }
 }
 $(document).ready(function(){
+    load_status();
     $(".userStatusText .statusText").focusin(function(){
         $(".userStatus").css("height","180px");
         $(".userStatusText .statusText").css("padding-bottom","80px");
@@ -60,7 +74,6 @@ $(document).ready(function(){
             $(".btnStatus ").css("display","none");
         }
     });
-    
     $(".statusText").keyup(function(e){
         if((event.keyCode === 13 ) && ($(".statusText").val()!=null) && ($(".statusText").val()!=""))
         postStatus();
